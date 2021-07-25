da3089ee09daca2c753d5929ff334e206aa1bacd
gclopes1978 <gclopes.developer@gmail.com>
2021-07-25 10:54:13 +0100

    Springboot Project Created. close #1

---
+++ src/main/java/com/gclopes/ProtoBuffServerAPP/ProtoBuffServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.gclopes.ProtoBuffServerAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoBuffServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoBuffServerAppApplication.class, args);
+    }
+
+}
